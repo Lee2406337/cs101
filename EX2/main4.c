620f5325ca534cea7106020e1ec82b8dab6470df
B1229011 <144075534+Lee2406337@users.noreply.github.com>
2023-12-14 14:04:08 +0800

    Create main4.c

---
+++ EX2/main4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 2100;
+    if (%400 == 0||(%4 == 0&& %100 !=0)){
+        print("閏年");
+    }
+    else{
+        print("平年");
+    }
+    return 0;
+}
