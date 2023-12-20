a86dfeb194297d6fb1f310f681d49e869ad15b93
B1229011 <144075534+Lee2406337@users.noreply.github.com>
2023-12-20 20:26:53 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -4,11 +4,12 @@ int main(){
     int n = 7;
     int spc = n;
     for (int i = 1; i <= n; i++){
-        for (int k = spc; k >= 1; k--){
-            printf(" ");
-        }
-        for (int j=1; j<=i; j++){
-            printf("%d ", i);
+        for (int j = spc; j >= 1; j--){
+            if (j > i) {
+                printf(" ");
+            } else {
+                printf("%d ", i);
+            }
         }
         printf("\n");
         spc--;
